3428b1f4ec040f3a5ac2e34bc04832dd2f499a3e
Mohamed Amine Azirgui <69221403+maaaz5@users.noreply.github.com>
2022-07-07 18:51:42 +0100

    buttons done!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+
+import Buttonoutlined from "./components/buttons/Buttonoutlined";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Buttonoutlined
+        fontSize="var(--S1)"
+        title="Click Me"
+        url="#"
+        right={true}
+      />
+    </div>
+  );
 }
 
 export default App;
